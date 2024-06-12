3fa9b351a1a0199daeeefcc025e063649ed4f600
RomanReznichenko <roma.reznichenko@smartexe.com>
2024-06-12 16:42:04 +0300

    add languages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,45 @@
-import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CUSTOM_ELEMENTS_SCHEMA, Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterOutlet } from "@angular/router";
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from "@angular/forms";
+import { map } from 'rxjs';
 
 interface WidgetForm {
   id: FormControl<string | null>;
   token: FormControl<string | null>;
+  language: FormControl<string>;
 }
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
   imports: [CommonModule, RouterOutlet, FormsModule, ReactiveFormsModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppComponent {
-  title = 'empty-angular';
+  title = "empty-angular";
   form: FormGroup<WidgetForm> = this.formBuilder.group({
-    id: new FormControl('', [Validators.required]),
-    token: new FormControl('', [Validators.required]),
+    id: new FormControl("", [Validators.required]),
+    token: new FormControl("", [Validators.required]),
+    language: new FormControl("en", { nonNullable: true }),
   });
-  
-  constructor(private formBuilder: FormBuilder) {  }
+
+  reload = true;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.form.valueChanges.subscribe(() => {
+      this.reload = false;
+
+      setTimeout(() => this.reload = true);
+    })
+  }
 }
