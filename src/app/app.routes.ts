f4e22ce9ff6e4ed78de2cc470ba49144f35ec9df
RomanReznichenko <roma.reznichenko@smartexe.com>
2024-06-12 14:47:50 +0300

    update widjet version and add inputs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,4 @@
 import { Routes } from '@angular/router';
-import { InGameComponent } from './components/in-game/in-game.component';
-import { InLobbyComponent } from './components/in-lobby/in-lobby.component';
 
 
-export const routes: Routes = [
-  {
-    path: 'in-game',
-    component: InGameComponent,
-  },
-  {
-    path: 'in-lobby',
-    component: InLobbyComponent,
-  },
-];
+export const routes: Routes = [];
